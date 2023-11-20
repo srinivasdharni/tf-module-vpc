f12b57644052610a004f1553633cd9cf98c377f4
India <India@India-PC>
2023-11-21 00:16:00 +0530

    m

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@
 	  }
 	}
 	
-		resource "aws_route_table" "app" {
+	resource "aws_route_table" "app" {
 	  vpc_id = aws_vpc.main.id
 	
 	  route {
@@ -126,6 +126,24 @@
 	
 	  tags = {
 	    Name = "app"
+	  }
+	}
+	
+	resource "aws_route_table" "db" {
+	  vpc_id = aws_vpc.main.id
+	
+	  route {
+	    cidr_block     = "0.0.0.0/0"
+	    nat_gateway_id = aws_nat_gateway.ngw.id
+	  }
+	
+	  route {
+	    cidr_block                = var.default_vpc_cidr
+	    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+	  }
+	
+	  tags = {
+	    Name = "db"
 	  }
 	}
 	
